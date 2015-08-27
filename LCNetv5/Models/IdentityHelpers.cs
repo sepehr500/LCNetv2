aabfbebc932db78ca0048dd79987afc2a1755a16
Sepher Sobhani <ssobhani@leidos.analytics.com>
2015-08-27 11:51:18 -0400

    Crap

---
+++ LCNetv5/Models/IdentityHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace LCNetv5.Models
+{
+    public static class IdentityHelpers
+    {
+        public static MvcHtmlString GetUserName(this HtmlHelper html, string id)
+        {
+            ApplicationUserManager mgr
+                = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            return new MvcHtmlString(mgr.FindByIdAsync(id).Result.UserName);
+        }
+    }
+}
