44f7debd37abb0903af0c1032c673680173aa62e
Sepher Sobhani <ssobhani@leidos.analytics.com>
2015-10-04 13:24:33 -0400

    Added SMS Functionality

---
+++ LCNetv5/Models/SendSMSViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LCNetv5.Models
+{
+    public class SendSMSViewModel
+    {
+        public List<string> Clients { get; set; }
+
+        public bool SendToAll { get; set; }
+
+
+    }
+}
